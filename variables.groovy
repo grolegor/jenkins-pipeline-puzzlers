ea0d844400e7131b76e90c5e2667e7c436c1e103
Egor Grol <grolegor@gmail.com>
2018-12-28 21:33:33 +0300

    add new puzzler about variables

---
+++ variables.groovy
@@ -0,0 +1,8 @@
+println("env.TESTVAR=$env.TESTVAR")
+println("testVar=$testVar")
+println("env.testVar += '-changed'")
+env.testVar += '-changed'
+println("env.testVar=$env.testVar")
+println("params.testVar=$params.testVar")
+println("params.TESTVAR=$params.TESTVAR")
+println("testVar=$testVar")
