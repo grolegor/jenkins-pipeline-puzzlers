078bc17c5ee02277d9e83e2b62089e7d6b83cd90
grol <grolegor@gmail.com>
2018-12-29 00:54:50 +0300

    add to String puzzler

--- to-string.groovy
+++ to-string.groovy
@@ -1,6 +1,8 @@
 @Library("sharedlib@to-string")
 import com.jenkins.CustomClass
 
+println(new CustomClass())
+
 println("without toString: " + new CustomClass())
 
 println("with toString: " + new CustomClass().toString())
